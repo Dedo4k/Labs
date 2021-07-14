781d05a48fb3ceadf931d551432f39a52e10c580
Dedo4k <70375483+Dedo4k@users.noreply.github.com>
2021-07-14 05:01:49 +0300

    Lab1

---
+++ КПИЯП/4_sem/Lab1/LAB_1_exe1.asm
@@ -0,0 +1,92 @@
+; multi-segment executable file template.
+
+data segment
+        start_msg db "Enter some string:",0Dh,0Ah,'$'  
+    output_msg db "You enter:$"
+    new_line db 0Dh,0Ah,'$'
+    buf label byte
+    pkey db "press any key...$"
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
+
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+
+mov ah,09h
+    lea dx,start_msg
+    int 21h;output start_msg in cmd   
+    
+    mov dx, offset buf   
+    mov ah, 0ah
+    int 21h;input string in buf
+    
+    mov ah,09h
+    lea dx, new_line
+    int 21h;new line
+    
+    mov ah,09h
+    lea dx, output_msg
+    int 21h;output output_msg in cmd
+    
+    mov	di,	offset buf          ;buf address
+    mov	bx,	0h        
+    mov	bl,	[di+1]	            ;string length
+    mov	byte ptr [di+bx+2], '$'	;address after string
+    mov	dx,	offset buf
+    add	dx,	2h
+    mov	ah,	9h
+    int	21h;output buf
+    
+generate_sound:;generating sound while any key will be pressed
+    call speaker_on
+    call delay
+    call speaker_off
+    call delay
+
+    mov ah,1
+    int 16h
+    jz generate_sound
+    mov ax,4C00h
+    int 21h
+                             
+speaker_on:
+    in al,61h
+    or al,2
+    out 61h,al
+    ret
+
+speaker_off:
+    in al, 61h
+    and al,11111100b
+    out 61h,al
+    ret
+
+delay:
+    mov cx,65535
+    ret
+
+_wait:
+    loop _wait
+    ret
+
+            
+    lea dx, pkey
+    mov ah, 9
+    int 21h        ; output string at ds:dx
+    
+    ; wait for any key....    
+    mov ah, 1
+    int 21h
+    
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+ends
+
+end start ; set entry point and stop the assembler.
